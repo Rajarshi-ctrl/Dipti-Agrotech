d462ba59a56e4422b39c7ac1b40ed5b7afa89900
Rajarshi Dutta <rajarshidutta99@gmail.com>
2022-05-23 17:42:20 +0530

    v2

--- agrotech/settings.py
+++ agrotech/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-so!kobr44d!l=1kf-3ymgg(xtx4^%7w$r-f(i%5x^il+kc8o0*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
